e98e888813dc245d0c1381e2d763d81889b0780e
Devninjap <perez_deyvi@hotmail.com>
2017-06-07 15:38:02 -0500

    up

---
+++ CapaNegocio/solicitudCN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidades;
+
+namespace CapaNegocio
+{
+    public class solicitudCN
+    {
+        //INSTANCIAS
+        egresadoCD obj = new egresadoCD();
+        //LISTAR EGRESADOS
+        public object getEgresado()
+        {
+            return obj.getEgresado();
+        }
+    }
+}
